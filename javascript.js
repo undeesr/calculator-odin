e941a69660606756a39ca4244e89fb8ca4074d1e
undeesr <tejassinghknp@gmail.com>
2022-12-30 11:58:40 +0530

    v0.0 Structure of calculator done

---
+++ javascript.js
@@ -0,0 +1,51 @@
+// cool gradient effect
+
+const gradient = Array.from(document.querySelectorAll("#gradient"));
+
+let counter = 0;
+
+function animateGradient() {
+  requestAnimationFrame(animateGradient);
+  counter++;
+  gradient.forEach((cell) => {
+    cell.style.background = `linear-gradient(${
+      counter % 360
+    }deg, transparent, rgba(255, 255, 255,0.5), rgba(255, 255, 255,0.5), #d6d4d4, rgba(255, 255, 255,0.5), rgba(255, 255, 255,0.5), transparent)`;
+    console.log(cell);
+  });
+
+  console.log(counter);
+}
+
+animateGradient();
+
+function operate(a, b, operator) {
+  if (b === 0 && operator === "/") {
+    return "lol";
+  }
+  let result = 0;
+
+  switch (operator) {
+    case "+":
+      result = a + b;
+      break;
+
+    case "-":
+      result = a - b;
+      break;
+
+    case "*":
+      result = a * b;
+      break;
+
+    case "/":
+      result = a / b;
+      break;
+
+    default:
+      result = "CRITICAL ERROR DEHFERIUGH";
+      break;
+  }
+
+  return result;
+}
